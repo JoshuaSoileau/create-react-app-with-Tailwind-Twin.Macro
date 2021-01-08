c5e7e9d2ae30f81ba3336332b642c30562386107
Joshua Soileau <joshua.soileau@gmail.com>
2021-01-08 09:53:57 -0500

    Adds hooks for useKeypress & useSound

---
+++ src/hooks/useKeypress.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+export default function useKeyPress(
+  targetKey,
+  onPressDown = () => {},
+  onPressUp = () => {}
+) {
+  // State for keeping track of whether key is pressed
+  const [keyPressed, setKeyPressed] = useState(false);
+
+  useEffect(() => {
+    // If pressed key is our target key then set to true
+    function downHandler({ key }) {
+      if (key === targetKey) {
+        setKeyPressed(true);
+        onPressDown();
+      }
+    }
+
+    // If released key is our target key then set to false
+    const upHandler = ({ key }) => {
+      if (key === targetKey) {
+        setKeyPressed(false);
+        onPressUp();
+      }
+    };
+
+    // Add event listeners
+    window.addEventListener("keydown", downHandler);
+    window.addEventListener("keyup", upHandler);
+
+    // Remove event listeners on cleanup
+    return () => {
+      window.removeEventListener("keydown", downHandler);
+      window.removeEventListener("keyup", upHandler);
+    };
+  });
+
+  return keyPressed;
+}
